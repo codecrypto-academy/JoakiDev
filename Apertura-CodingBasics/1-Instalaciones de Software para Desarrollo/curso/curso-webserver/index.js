637e8d382fba09e807e1605c6e993821edab3565
JoakiDev <joakidev@gmail.com>
2024-11-03 12:43:01 +0100

    feat: coding basics

---
+++ Apertura-CodingBasics/1-Instalaciones de Software para Desarrollo/curso/curso-webserver/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/hora', (req, res) => {
+    res.send(new Date());
+});
+
+app.listen(3000)
